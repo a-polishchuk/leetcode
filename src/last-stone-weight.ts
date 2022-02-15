5f1c6191124345863eb98c2dccd82b6abb4c1bbc
Andrii Polishchuk <polishchuk91@gmail.com>
2022-02-15 12:37:22 +0200

    Last stone weight, sorted array + binary search

---
+++ src/last-stone-weight.ts
@@ -0,0 +1,29 @@
+// NOTE: splice operation is pretty heavy, we can use MaxHeap instead
+
+function findInsertIndex(sorted: number[], target: number): number {
+  let low = 0;
+  let high = sorted.length - 1;
+  while (low < high) {
+    const mid = Math.floor((low + high) / 2);
+    if (sorted[mid] == target) {
+      return mid;
+    } else if (sorted[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return sorted[low] < target ? low + 1 : low;
+}
+
+function lastStoneWeight(stones: number[]): number {
+  stones.sort((a, b) => a - b);
+  while (stones.length > 1) {
+    const diff = stones.pop()! - stones.pop()!;
+    if (diff > 0) {
+      const insertIndex = findInsertIndex(stones, diff);
+      stones.splice(insertIndex, 0, diff);
+    }
+  }
+  return stones[0] || 0;
+}
